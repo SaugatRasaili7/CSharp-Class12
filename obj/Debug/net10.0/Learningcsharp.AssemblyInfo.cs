6c9c87ee813287201d6d3f1c047e3c083519d243
Saugat Rasaili <saugatrasaili76@gmail.com>
2026-06-11 01:01:50 -0700

    first commit

--- obj/Debug/net10.0/Learningcsharp.AssemblyInfo.cs
+++ obj/Debug/net10.0/Learningcsharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Learningcsharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df0bcd76ad58e3c565b95019165d3e78f664dd8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Learningcsharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Learningcsharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
